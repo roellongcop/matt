7fa3bf3e30caa8fac38daccaface8b41d8ced351
roellongcop <longcoproel@gmail.com>
2023-11-01 21:46:19 +0800

    c

--- views/site/frontend/team.php
+++ views/site/frontend/team.php
@@ -9,17 +9,17 @@ use app\helpers\App;
       <h2 class="centered">Meet the Team</h2>
       <hr>
 	   <div class="col-lg-8 col-lg-offset-2 centered">
-        <p>Behind every great platform is a dedicated team. Get to know the passionate individuals who drive our mission forward:</p>
+        <p>Behind every great platform is a dedicated team. Get to know the passionate individuals who drive our mission forward</p>
 		<br/>
       </div>
       <div class="col-lg-3 centered"> 
         <img class="img img-circle" src="<?= App::publishedUrl('/img/team/default.png', '@app/assets/frontend') ?>" height="120px" width="120px" alt="">
         <h4><strong>John Williams</strong></h4>
-        <p>Lead Spiritual Advisor with over a decade of guiding communal prayers.</p>
+        <p>Lead Spiritual Advisor with over a decade of guiding prayers.</p>
         <a href="#"><i class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i class="fa fa-linkedin"></i></a> </div>
       <div class="col-lg-3 centered"> <img class="img img-circle" src="<?= App::publishedUrl('/img/team/default.png', '@app/assets/frontend') ?>" height="120px" width="120px" alt="">
         <h4><b>Sophia Martinez</b></h4>
-        <p>Digital Engagement Expert passionate about connecting believers worldwide.</p>
+        <p>Digital Engagement Expert connecting believers worldwide.</p>
         <a href="#"><i class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i class="fa fa-linkedin"></i></a> </div>
       <div class="col-lg-3 centered"> <img class="img img-circle" src="<?= App::publishedUrl('/img/team/default.png', '@app/assets/frontend') ?>" height="120px" width="120px" alt="">
         <h4><b>Liam Thompson</b></h4>
@@ -27,7 +27,7 @@ use app\helpers\App;
         <a href="#"><i class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i class="fa fa-linkedin"></i></a> </div>
       <div class="col-lg-3 centered"> <img class="img img-circle" src="<?= App::publishedUrl('/img/team/default.png', '@app/assets/frontend') ?>" height="120px" width="120px" alt="">
         <h4><b>Emily Clark</b></h4>
-        <p>Community Outreach Specialist dedicated to fostering strong bonds among members.</p>
+        <p>Community Outreach Specialist fostering bonds among members.</p>
         <a href="#"><i class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i class="fa fa-linkedin"></i></a> </div>
     </div>
   </div>
